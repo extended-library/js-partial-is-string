deba018d94a566ba1882ceafd7bd1dd95f27fa5f
Richard King <richrdkng@gmail.com>
2016-08-18 22:45:04 +0000

    0.0.1

--- dist/js-partial-is-string.js
+++ dist/js-partial-is-string.js
@@ -8,7 +8,7 @@
  * More information on [JavaScript Open Standards]{@link https://github.com/jsopenstd/jsopenstd}.
  *
  * @namespace js.partial
- * @version 0.0.0
+ * @version 0.0.1
  *
  * @author Richard King <richrdkng@gmail.com> [GitHub]{@link https://github.com/richrdkng}
  * @license [MIT]{@link https://github.com/jsopenstd/js-partial-foreach/blob/master/license.md}
